785c32e6f8d4a41bb5cfc0894325108f9ac67421
Michael Baxter <baxt01@hotmail.com>
2024-01-28 21:25:09 +0000

    called the weather icon code into a variable and built the link to get the icon onto the page and added the content into todays weather, ready to create the history section to hold the buttons for the historical searches then I will build the future weather reports.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var API = "0faefdf2ca0e5811213ef3ffb85306f1";
+var iconurl = 'https://openweathermap.org/img/w/';
 
 // set the onclick function.
 $('#search-button').on('click', function (e) {
@@ -12,8 +13,7 @@ $('#search-button').on('click', function (e) {
     var location = $('#search-input').val();
 
     var geoQueryurlq = geoQueryURL + 'q=' + location + '&limit=5&appid=' + API;
-    console.log(geoQueryurlq);
-
+    var date = dayjs().format('DD-MM-YYYY');
 
     // Here we run our Fetch call to the OpenWeatherMap geolocation API
     fetch(geoQueryurlq)
@@ -24,22 +24,16 @@ $('#search-button').on('click', function (e) {
     // We store all of the retrieved data inside of an object called "data"
     .then(function (data) {
 
-        // Log the resulting object
-        console.log(data);
-
         // retrieving the LAt & lon coordinates from the geolocation on open weather map.
         var lat = data[0].lat;
-        console.log(lat);
         var lon = data[0].lon;
-        console.log(lon);
 
         // base URL for the weather call.
-        var queryURL = 'https://api.openweathermap.org/data/2.5/weather?';
-
+         var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?';
+        
         // building the URL for the api call.
-        var queryurlq = `${queryURL}lat=${lat}&lon=${lon}&appid=${API}`;
-        // logging the URL to the console.
-        console.log(queryurlq);
+        var queryurlq = `${queryURL}lat=${lat}&lon=${lon}&appid=${API}&units=metric`;
+
         // Here we run the fetch call to the open weather API.
         fetch(queryurlq)
         .then(function(res){
@@ -47,9 +41,25 @@ $('#search-button').on('click', function (e) {
             return res.json();
         })
         .then(function(data){
-            // logging the stored data inside the data object.
-            console.log(data);
+            // putting the current weather icon on the page.
+            ''
+            var iconCode = data.list[0].weather[0].icon;
+            var icon = `${iconurl}${iconCode}`;
+            console.log(data);  
+            var city = data.city.name;
+            var temp = data.list[0].main.temp;
+            var wind = data.list[0].wind.speed;
+            var humidity = data.list[0].main.humidity;
+            
+            // adding the wether for Today onto the page.
+            $('#today').html(`<h1>${city} ${date}<img src=${icon}.png></h1> <br> 
+            <p>Temp: ${temp}°c <br>
+             Wind: ${wind}MPH <br>
+             Humidity: ${humidity}% </p>`);
 
+             //creating the buttons for history.
+             
+        
         })
 
     })
