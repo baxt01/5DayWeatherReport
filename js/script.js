4e0808ca10e6f544e747beb3d24cf040258be63c
Michael Baxter <baxt01@hotmail.com>
2024-01-30 15:03:17 +0000

    everything in the code is completed except the history buttons do not have a click listener

--- js/script.js
+++ js/script.js
@@ -16,12 +16,13 @@ for (var i = 0; i < parsedRecovery.length - 1; i++) {
     historicalSearch.attr('id', place);
     historicalSearch.text(place);
     $('#history').append(historicalSearch);
+    
 }
 }
 
 
 // set the onclick function.
-$('#search-button').on('click', function (e) {
+$('#search-button').on('click', function start (e) {
     // prevent the default browser behavior.
     e.preventDefault();
 
@@ -68,7 +69,7 @@ $('#search-button').on('click', function (e) {
             var temp = data.list[0].main.temp;
             var wind = data.list[0].wind.speed;
             var humidity = data.list[0].main.humidity;
-            
+            console.log(data);
                     // push current city to the array in local storage.
                    recover.push(city);
                    var rec = recover.concat(parsedRecovery);
@@ -99,9 +100,102 @@ $('#search-button').on('click', function (e) {
             historicalSearch.addClass('btn btn-secondary mb-2');
             historicalSearch.attr('data-name', city);
             historicalSearch.text(city);
-            $('#history').append(historicalSearch);
-
+            $('#history').append(historicalSearch).click(function (e) {
+                start(e);
+            });
+           
             // Building the forward forecast.
+            x =  [7, 15, 23, 31, 39]
+
+                var iconCode = data.list[7].weather[0].icon;
+                var icon1 = `${iconurl}${iconCode}`;
+                var city1 = data.city.name;
+                var temp1 = data.list[7].main.temp;
+                var wind1 = data.list[7].wind.speed;
+                var humidity1 = data.list[7].main.humidity;
+                
+                var iconCode = data.list[15].weather[0].icon;
+                var icon2 = `${iconurl}${iconCode}`;
+                var city2 = data.city.name;
+                var temp2 = data.list[15].main.temp;
+                var wind2 = data.list[15].wind.speed;
+                var humidity2 = data.list[15].main.humidity;
+                
+                var iconCode = data.list[23].weather[0].icon;
+                var icon3 = `${iconurl}${iconCode}`;
+                var city3 = data.city.name;
+                var temp3 = data.list[23].main.temp;
+                var wind3 = data.list[23].wind.speed;
+                var humidity3 = data.list[23].main.humidity;
+                
+                var iconCode = data.list[31].weather[0].icon;
+                var icon4 = `${iconurl}${iconCode}`;
+                var city4 = data.city.name;
+                var temp4 = data.list[31].main.temp;
+                var wind4 = data.list[31].wind.speed;
+                var humidity4 = data.list[31].main.humidity;
+                
+                var iconCode = data.list[39].weather[0].icon;
+                var icon5 = `${iconurl}${iconCode}`;
+                var city5 = data.city.name;
+                var temp5 = data.list[39].main.temp;
+                var wind5 = data.list[39].wind.speed;
+                var humidity5 = data.list[39].main.humidity;
+                
+
+                 $('#forecast').html(`<p>5 day Forecast:</><br>
+                 <div class="card bg-dark text-light me-3" style="width: 10rem">
+                 <div class="card-body">
+                 <h5 class="card-title">${dayjs().add(1, "day").format("DD-MM-YYYY")}</h5>
+                 <img src="${icon1}.png">
+                 <div class="card-text"> Temp: ${temp1}°c<br>
+                 Wind: ${wind1}MPH<br>
+                 Humidity: ${humidity1}%
+                 </div>
+                 </div>
+                 </div><br>
+                 <div class="card bg-dark text-light me-3" style="width: 10rem">
+                 <div class="card-body">
+                 <h5 class="card-title">${dayjs().add(2, "day").format("DD-MM-YYYY")}</h5>
+                 <img src="${icon2}.png">
+                 <div class="card-text"> Temp: ${temp2}°c<br>
+                 Wind: ${wind2}MPH<br>
+                 Humidity: ${humidity2}%
+                 </div>
+                 </div>
+                 </div><br>
+                 <div class="card bg-dark text-light me-3" style="width: 10rem">
+                 <div class="card-body">
+                 <h5 class="card-title">${dayjs().add(3, "day").format("DD-MM-YYYY")}</h5>
+                 <img src="${icon3}.png">
+                 <div class="card-text"> Temp: ${temp3}°c<br>
+                 Wind: ${wind3}MPH<br>
+                 Humidity: ${humidity3}%
+                 </div>
+                 </div>
+                 </div><br>
+                 <div class="card bg-dark text-light me-3" style="width: 10rem">
+                 <div class="card-body">
+                 <h5 class="card-title">${dayjs().add(4, "day").format("DD-MM-YYYY")}</h5>
+                 <img src="${icon4}.png">
+                 <div class="card-text"> Temp: ${temp4}°c<br>
+                 Wind: ${wind4}MPH<br>
+                 Humidity: ${humidity4}%
+                 </div>
+                 </div>
+                 </div><br>
+                 <div class="card bg-dark text-light me-3" style="width: 10rem">
+                 <div class="card-body">
+                 <h5 class="card-title">${dayjs().add(5, "day").format("DD-MM-YYYY")}</h5>
+                 <img src="${icon5}.png">
+                 <div class="card-text"> Temp: ${temp5}°c<br>
+                 Wind: ${wind5}MPH<br>
+                 Humidity: ${humidity5}%
+                 </div>
+                 </div>
+                 </div><br>
+                 `)
+                 
         })
         
     })
