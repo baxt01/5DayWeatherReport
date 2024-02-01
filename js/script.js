4cee7ad50abb27c97b5a674bcf275d251a11a52e
Michael Baxter <baxt01@hotmail.com>
2024-02-01 13:11:31 +0000

    I have refactored my code into functions

--- js/script.js
+++ js/script.js
@@ -1,206 +1,195 @@
 var API = "0faefdf2ca0e5811213ef3ffb85306f1";
 var iconurl = 'https://openweathermap.org/img/w/';
 
-// Recover the local storage.
 var recover = [];
 var recovery = localStorage.getItem('city');
-parsedRecovery = JSON.parse(recovery);
+var parsedRecovery = JSON.parse(recovery);
+
 if (!parsedRecovery) {
     console.log('empty');
 } else {
-    //loop to get the history button on the page.
-for (var i = 0; i < parsedRecovery.length - 1; i++) {
-    var place = parsedRecovery[i];
-    var historicalSearch = $('<button>');
-    historicalSearch.addClass('btn btn-secondary mb-2');
-    historicalSearch.attr('id', place);
-    historicalSearch.text(place);
-    $('#history').append(historicalSearch);
-    
-}
+    for (var i = 0; i < parsedRecovery.length - 1; i++) {
+        var place = parsedRecovery[i];
+        var historicalSearch = $('<button>');
+        historicalSearch.addClass('btn btn-secondary mb-2');
+        historicalSearch.attr('id', place);
+        historicalSearch.text(place);
+        $('#history').append(historicalSearch);
+    }
 }
 
-
-// set the onclick function.
-$('#search-button').on('click', function start (e) {
-    // prevent the default browser behavior.
+$('#search-button').on('click', function (e) {
     e.preventDefault();
 
-    // Here we are building the URL we need to query the database
-    var geoQueryURL = "https://api.openweathermap.org/geo/1.0/direct?";
-    
-    // get the user's inputted location.
-    var location = $('#search-input').val();
+    var location = $('#search-input').val();  // Get the location from the input field
+    if (!location) {
+        console.log('Empty location input');
+        return;
+    }
 
-    // Building the geo location URL.
+    var geoQueryURL = "https://api.openweathermap.org/geo/1.0/direct?";
     var geoQueryurlq = geoQueryURL + 'q=' + location + '&limit=5&appid=' + API;
     var date = dayjs().format('DD-MM-YYYY');
-    
-    // Here we run our Fetch call to the OpenWeatherMap geolocation API
+
+    convertGeo(geoQueryurlq);
+});
+
+function start(e) {
+    e.preventDefault();
+    var location = $(this).attr('data-name');
+    var geoQueryURL = "https://api.openweathermap.org/geo/1.0/direct?";
+    var geoQueryurlq = geoQueryURL + 'q=' + location + '&limit=5&appid=' + API;
+    convertGeo(geoQueryurlq);
+}
+
+function convertGeo(geoQueryurlq) {
     fetch(geoQueryurlq)
-    .then(function (response) {
-        // Calling .json() to access the json data stored inside the returned promise
-        return response.json();
-    })
-    // We store all of the retrieved data inside of an object called "data"
-    .then(function (data) {
-
-        // retrieving the LAt & lon coordinates from the geolocation on open weather map.
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-
-        // base URL for the weather call.
-         var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?';
-        
-        // building the URL for the api call.
-        var queryurlq = `${queryURL}lat=${lat}&lon=${lon}&appid=${API}&units=metric`;
-
-        // Here we run the fetch call to the open weather API.
-        fetch(queryurlq)
-        .then(function(res){
-            // calling the .json()to access the json data stored inside the returned promise.
-            return res.json();
+        .then(function (response) {
+            return response.json();
         })
-        .then(function(data){
-            // putting the current weather & icon on the page.
-            var iconCode = data.list[0].weather[0].icon;
-            var icon = `${iconurl}${iconCode}`;
-            var city = data.city.name;
-            var temp = data.list[0].main.temp;
-            var wind = data.list[0].wind.speed;
-            var humidity = data.list[0].main.humidity;
-            console.log(data);
-                    // push current city to the array in local storage.
-                   recover.push(city);
-                   var rec = recover.concat(parsedRecovery);
-                   // Removing Duplicate entries so on refresh or re-visit there is only one of each location name. 
-                   function removeDuplicates (rec) {
-                    let unique = rec.reduce(function (acc, curr) {
-                        if (!acc.includes(curr))
-                        acc.push(curr);
-                    return acc;
-                    }, newRec = []);
-                   }
-                   removeDuplicates(rec);
+        .then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var queryURL = 'https://api.openweathermap.org/data/2.5/forecast?';
+            var queryurlq = `${queryURL}lat=${lat}&lon=${lon}&appid=${API}&units=metric`;
 
-                    //use JSON stringify to convert the array.
+            fetch(queryurlq)
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (data) {
+                    var iconCode = data.list[0].weather[0].icon;
+                    var icon = `${iconurl}${iconCode}`;
+                    var city = data.city.name;
+                    var temp = data.list[0].main.temp;
+                    var wind = data.list[0].wind.speed;
+                    var humidity = data.list[0].main.humidity;
+                    var GetDate = data.list[0].dt_txt;
+                    var date = GetDate.slice(0, 10);
+                    console.log(date);
+                    recover.push(city);
+                    var rec = recover.concat(parsedRecovery);
+                    function removeDuplicates(rec) {
+                        let unique = rec.reduce(function (acc, curr) {
+                            if (!acc.includes(curr))
+                                acc.push(curr);
+                            return acc;
+                        }, []);
+                        return unique;
+                    }
+                    var newRec = removeDuplicates(rec);
                     var saving = JSON.stringify(newRec);
+                    localStorage.setItem('city', saving);
 
-                    // store the city name in local storage to recreate the history buttons.
-                        localStorage.setItem('city', saving);
+                    $('#today').html(`<h1>${city} ${date}<img src=${icon}.png></h1> <br> 
+                    <p>Temp: ${temp}°c <br> Wind: ${wind}MPH <br> Humidity: ${humidity}% </p>`);
 
-            // adding the wether for Today onto the page.
-            $('#today').html(`<h1>${city} ${date}<img src=${icon}.png></h1> <br> 
-            <p>Temp: ${temp}°c <br>
-             Wind: ${wind}MPH <br>
-             Humidity: ${humidity}% </p>`);
-          
-             //creating the buttons for history.
-            var historicalSearch = $('<button>');
-            historicalSearch.addClass('btn btn-secondary mb-2');
-            historicalSearch.attr('data-name', city);
-            historicalSearch.text(city);
-            $('#history').append(historicalSearch).click(function (e) {
-                start(e);
-            });
-           
-            // Building the forward forecast.
+                    var historicalSearch = $('<button>');
+                    historicalSearch.addClass('btn btn-secondary mb-2');
+                    historicalSearch.attr('data-name', city);
+                    historicalSearch.text(city);
+                    $('#history').append(historicalSearch).click(function (e) {
+                        start(e);
+                    });
+
+                     // Building the forward forecast.
             x =  [7, 15, 23, 31, 39]
 
-                var iconCode = data.list[7].weather[0].icon;
-                var icon1 = `${iconurl}${iconCode}`;
-                var city1 = data.city.name;
-                var temp1 = data.list[7].main.temp;
-                var wind1 = data.list[7].wind.speed;
-                var humidity1 = data.list[7].main.humidity;
-                
-                var iconCode = data.list[15].weather[0].icon;
-                var icon2 = `${iconurl}${iconCode}`;
-                var city2 = data.city.name;
-                var temp2 = data.list[15].main.temp;
-                var wind2 = data.list[15].wind.speed;
-                var humidity2 = data.list[15].main.humidity;
-                
-                var iconCode = data.list[23].weather[0].icon;
-                var icon3 = `${iconurl}${iconCode}`;
-                var city3 = data.city.name;
-                var temp3 = data.list[23].main.temp;
-                var wind3 = data.list[23].wind.speed;
-                var humidity3 = data.list[23].main.humidity;
-                
-                var iconCode = data.list[31].weather[0].icon;
-                var icon4 = `${iconurl}${iconCode}`;
-                var city4 = data.city.name;
-                var temp4 = data.list[31].main.temp;
-                var wind4 = data.list[31].wind.speed;
-                var humidity4 = data.list[31].main.humidity;
-                
-                var iconCode = data.list[39].weather[0].icon;
-                var icon5 = `${iconurl}${iconCode}`;
-                var city5 = data.city.name;
-                var temp5 = data.list[39].main.temp;
-                var wind5 = data.list[39].wind.speed;
-                var humidity5 = data.list[39].main.humidity;
-                
+            var iconCode = data.list[7].weather[0].icon;
+            var icon1 = `${iconurl}${iconCode}`;
+            var city1 = data.city.name;
+            var temp1 = data.list[7].main.temp;
+            var wind1 = data.list[7].wind.speed;
+            var humidity1 = data.list[7].main.humidity;
+            
+            var iconCode = data.list[15].weather[0].icon;
+            var icon2 = `${iconurl}${iconCode}`;
+            var city2 = data.city.name;
+            var temp2 = data.list[15].main.temp;
+            var wind2 = data.list[15].wind.speed;
+            var humidity2 = data.list[15].main.humidity;
+            
+            var iconCode = data.list[23].weather[0].icon;
+            var icon3 = `${iconurl}${iconCode}`;
+            var city3 = data.city.name;
+            var temp3 = data.list[23].main.temp;
+            var wind3 = data.list[23].wind.speed;
+            var humidity3 = data.list[23].main.humidity;
+            
+            var iconCode = data.list[31].weather[0].icon;
+            var icon4 = `${iconurl}${iconCode}`;
+            var city4 = data.city.name;
+            var temp4 = data.list[31].main.temp;
+            var wind4 = data.list[31].wind.speed;
+            var humidity4 = data.list[31].main.humidity;
+            
+            var iconCode = data.list[39].weather[0].icon;
+            var icon5 = `${iconurl}${iconCode}`;
+            var city5 = data.city.name;
+            var temp5 = data.list[39].main.temp;
+            var wind5 = data.list[39].wind.speed;
+            var humidity5 = data.list[39].main.humidity;
+            
 
-                 $('#forecast').html(`<p>5 day Forecast:</><br>
-                 <div class="card bg-dark text-light me-3" style="width: 10rem">
-                 <div class="card-body">
-                 <h5 class="card-title">${dayjs().add(1, "day").format("DD-MM-YYYY")}</h5>
-                 <img src="${icon1}.png">
-                 <div class="card-text"> Temp: ${temp1}°c<br>
-                 Wind: ${wind1}MPH<br>
-                 Humidity: ${humidity1}%
-                 </div>
-                 </div>
-                 </div><br>
-                 <div class="card bg-dark text-light me-3" style="width: 10rem">
-                 <div class="card-body">
-                 <h5 class="card-title">${dayjs().add(2, "day").format("DD-MM-YYYY")}</h5>
-                 <img src="${icon2}.png">
-                 <div class="card-text"> Temp: ${temp2}°c<br>
-                 Wind: ${wind2}MPH<br>
-                 Humidity: ${humidity2}%
-                 </div>
-                 </div>
-                 </div><br>
-                 <div class="card bg-dark text-light me-3" style="width: 10rem">
-                 <div class="card-body">
-                 <h5 class="card-title">${dayjs().add(3, "day").format("DD-MM-YYYY")}</h5>
-                 <img src="${icon3}.png">
-                 <div class="card-text"> Temp: ${temp3}°c<br>
-                 Wind: ${wind3}MPH<br>
-                 Humidity: ${humidity3}%
-                 </div>
-                 </div>
-                 </div><br>
-                 <div class="card bg-dark text-light me-3" style="width: 10rem">
-                 <div class="card-body">
-                 <h5 class="card-title">${dayjs().add(4, "day").format("DD-MM-YYYY")}</h5>
-                 <img src="${icon4}.png">
-                 <div class="card-text"> Temp: ${temp4}°c<br>
-                 Wind: ${wind4}MPH<br>
-                 Humidity: ${humidity4}%
-                 </div>
-                 </div>
-                 </div><br>
-                 <div class="card bg-dark text-light me-3" style="width: 10rem">
-                 <div class="card-body">
-                 <h5 class="card-title">${dayjs().add(5, "day").format("DD-MM-YYYY")}</h5>
-                 <img src="${icon5}.png">
-                 <div class="card-text"> Temp: ${temp5}°c<br>
-                 Wind: ${wind5}MPH<br>
-                 Humidity: ${humidity5}%
-                 </div>
-                 </div>
-                 </div><br>
-                 `)
-                 
+             $('#forecast').html(`<p>5 day Forecast:</><br>
+             <div class="card bg-dark text-light me-3" style="width: 10rem">
+             <div class="card-body">
+             <h5 class="card-title">${dayjs().add(1, "day").format("DD-MM-YYYY")}</h5>
+             <img src="${icon1}.png">
+             <div class="card-text"> Temp: ${temp1}°c<br>
+             Wind: ${wind1}MPH<br>
+             Humidity: ${humidity1}%
+             </div>
+             </div>
+             </div><br>
+             <div class="card bg-dark text-light me-3" style="width: 10rem">
+             <div class="card-body">
+             <h5 class="card-title">${dayjs().add(2, "day").format("DD-MM-YYYY")}</h5>
+             <img src="${icon2}.png">
+             <div class="card-text"> Temp: ${temp2}°c<br>
+             Wind: ${wind2}MPH<br>
+             Humidity: ${humidity2}%
+             </div>
+             </div>
+             </div><br>
+             <div class="card bg-dark text-light me-3" style="width: 10rem">
+             <div class="card-body">
+             <h5 class="card-title">${dayjs().add(3, "day").format("DD-MM-YYYY")}</h5>
+             <img src="${icon3}.png">
+             <div class="card-text"> Temp: ${temp3}°c<br>
+             Wind: ${wind3}MPH<br>
+             Humidity: ${humidity3}%
+             </div>
+             </div>
+             </div><br>
+             <div class="card bg-dark text-light me-3" style="width: 10rem">
+             <div class="card-body">
+             <h5 class="card-title">${dayjs().add(4, "day").format("DD-MM-YYYY")}</h5>
+             <img src="${icon4}.png">
+             <div class="card-text"> Temp: ${temp4}°c<br>
+             Wind: ${wind4}MPH<br>
+             Humidity: ${humidity4}%
+             </div>
+             </div>
+             </div><br>
+             <div class="card bg-dark text-light me-3" style="width: 10rem">
+             <div class="card-body">
+             <h5 class="card-title">${dayjs().add(5, "day").format("DD-MM-YYYY")}</h5>
+             <img src="${icon5}.png">
+             <div class="card-text"> Temp: ${temp5}°c<br>
+             Wind: ${wind5}MPH<br>
+             Humidity: ${humidity5}%
+             </div>
+             </div>
+             </div><br>
+             `)
+                })
+                .catch(function (error) {
+                    console.log('Error fetching weather data: ', error);
+                });
         })
-        
-    })
-    
-})
-
-                
+        .catch(function (error) {
+            console.log('Error fetching geolocation data: ', error);
+        });
+}
 
